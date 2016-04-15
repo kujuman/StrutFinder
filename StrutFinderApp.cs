b0bdc71ac5e287a203c91ef1f86282d5b58c73f8
Jonathan Bayer <linuxgurugamer@gmail.com>
2016-04-15 11:19:44 -0400

    Added window to show struts Log is there to delete a single strut, but the delete isn't working yet

--- StrutFinderApp.cs
+++ StrutFinderApp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using KSP;
 using UnityEngine;
 using KSP.UI.Screens;
@@ -24,6 +24,139 @@ namespace StrutFinder
         List<Part> badStruts = null;
         List<Part> badFuelLines = null;
 
+		bool display = false;
+
+		private Rect strutwin;
+		const float WIDTH = 500.0f;
+		const float HEIGHT = 250.0f;
+		void Start()
+		{
+			strutwin = new Rect ((float)(Screen.width- WIDTH), (float)(Screen.height / 2.0 - HEIGHT), WIDTH, HEIGHT);
+		}
+
+		void OnGUI()
+		{
+			//Debug.Log ("display: " + display.ToString ());
+			if (display)
+				strutwin = GUILayout.Window (this.GetInstanceID () + 1, strutwin, new GUI.WindowFunction (draw), "Struts & Fuel Ducts", new GUILayoutOption[0]);
+		}
+			
+
+		public Vector2 sitesScrollPosition;
+		bool goodFirst = true;
+		GUIStyle bstyle = new GUIStyle (HighLogic.Skin.button);
+
+		enum lineType{goodFuel, goodStrut, badFuel, badStrut};
+		Part selectedStrut = null;
+
+		void showSelectedPart(lineType type, Part part)
+		{
+			UnHighlightParts(goodFuelLines);
+			UnHighlightParts(goodStruts);
+			UnHighlightParts( badFuelLines);
+			UnHighlightParts(badStruts);
+			selectedStrut = part;
+
+			switch (type) {
+			case lineType.goodFuel:
+				HighlightSinglePart (XKCDColors.Aqua, XKCDColors.Yellow, part);
+				break;
+			case lineType.goodStrut:
+				HighlightSinglePart (XKCDColors.Purple, XKCDColors.OffWhite, part);
+				break;
+			case lineType.badFuel:
+				HighlightSinglePart (XKCDColors.Aqua, XKCDColors.Green, part);
+				break;
+			case lineType.badStrut:
+				HighlightSinglePart (XKCDColors.Purple, XKCDColors.Red, part);
+				break;
+			}
+		}
+
+		void showGoodStruts()
+		{
+			//  list the good struts
+			bstyle.normal.textColor = Color.green;
+
+			foreach (Part part in goodFuelLines) {
+				GUILayout.BeginHorizontal ();
+				if (GUILayout.Button (part.ToString (), bstyle)) {
+					showSelectedPart (lineType.goodFuel,  part);
+				}
+				GUILayout.EndHorizontal ();
+			}
+			foreach (Part part in goodStruts) {
+				GUILayout.BeginHorizontal ();
+					if (GUILayout.Button (part.ToString (), bstyle)) {
+					showSelectedPart (lineType.goodStrut, part);
+					}
+				GUILayout.EndHorizontal ();
+			}
+		}
+		void showBadStruts()
+		{
+			// show the bad struts
+			bstyle.normal.textColor = Color.red;
+			foreach (Part part in badFuelLines) {
+				GUILayout.BeginHorizontal ();
+					if (GUILayout.Button (part.ToString (), bstyle)) {
+						showSelectedPart (lineType.badFuel,  part);
+					}
+				GUILayout.EndHorizontal ();
+			}
+			foreach (Part part in badStruts) {
+				GUILayout.BeginHorizontal ();
+					if (GUILayout.Button (part.ToString (), bstyle)) {
+						showSelectedPart (lineType.badStrut, part);
+					}
+				GUILayout.EndHorizontal ();
+			}
+		}
+
+		void draw(int id)
+		{
+			GUI.skin = HighLogic.Skin;
+			GUILayout.Label ("Click button to highlight strut for deletion");
+			sitesScrollPosition = GUILayout.BeginScrollView (sitesScrollPosition);
+			if (goodFirst) {
+				showGoodStruts ();
+				showBadStruts ();
+			} else {
+				showBadStruts ();
+				showGoodStruts ();
+			}
+
+			GUILayout.EndScrollView ();
+			GUILayout.BeginHorizontal ();
+			if (GUILayout.Button ("Hide Window")) {
+				display = false;
+			}
+			if (goodFirst) {
+				if (GUILayout.Button ("Show bad struts first")) {
+					selectedStrut = null;
+					goodFirst = false;
+				}
+			} else {
+				if (GUILayout.Button ("Show good struts first")) {
+					selectedStrut = null;
+					goodFirst = true;
+				}
+			}
+			if (selectedStrut != null) {
+				if (GUILayout.Button ("Delete Selected part")) {
+					Debug.Log ("Delete Selected part");
+					// Following logic is not working, it is failing on DeletePart
+//					EditorLogic.SortedShipList.Remove (selectedStrut);
+
+//					EditorLogic.DeletePart (selectedStrut);
+//					EditorLogic.Destroy (selectedStrut);
+					selectedStrut = null;
+					
+				}
+			}
+			GUILayout.EndHorizontal ();
+			GUI.DragWindow ();
+		}
         void OnGUIAppLauncherReady()
         {
             if (ApplicationLauncher.Ready)
@@ -48,10 +181,13 @@ namespace StrutFinder
         void TurnHighlightOff()
         {
             ToggleHighlight(false);
+
         }
 
         void ToggleHighlight(bool state)
         {
+			display = state;
+			selectedStrut = null;
             switch (state)
             {
                 case true:
@@ -129,6 +265,7 @@ namespace StrutFinder
 
             foreach (CompoundPart p in vesselP.OfType<CompoundPart>())
             {
+				
                 switch (p.name)
                 {
                     case "fuelLine":
@@ -174,16 +311,28 @@ namespace StrutFinder
             attached.Add(part);
         }
 
+		void HighlightSinglePart(Color highlightC, Color edgeHighlightColor, Part p)
+		{
+			p.SetHighlightDefault();
+			p.SetHighlightType(Part.HighlightType.AlwaysOn);
+			p.SetHighlight(true, false);
+			p.SetHighlightColor(highlightC);
+			p.highlighter.ConstantOn(edgeHighlightColor);
+			p.highlighter.SeeThroughOn();
+		}
         void HighlightParts(Color highlightC, Color edgeHighlightColor, List<Part> partList)
         {
                 foreach (Part p in partList)
                 {
+				HighlightSinglePart (highlightC, edgeHighlightColor, p);
+				#if false
                    p.SetHighlightDefault();
                    p.SetHighlightType(Part.HighlightType.AlwaysOn);
                    p.SetHighlight(true, false);
                    p.SetHighlightColor(highlightC);
                    p.highlighter.ConstantOn(edgeHighlightColor);
                    p.highlighter.SeeThroughOn();
+				#endif
                 }
         }
 
